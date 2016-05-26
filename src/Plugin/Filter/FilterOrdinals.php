0c39701fdd4cf5305a00e54ff07390e6585d47c1
Rian Callahan <rc@riancallahan.com>
2016-05-26 12:58:12 -0700

    Added settings form for ordinals filter, incorporated statically-cached settings into filter callback fn. Modified text and descriptions.

--- src/Plugin/Filter/FilterOrdinals.php
+++ src/Plugin/Filter/FilterOrdinals.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\more_filters\Plugin\Filter;
 
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\filter\FilterProcessResult;
 use Drupal\filter\Plugin\FilterBase;
 
@@ -24,7 +25,7 @@ class FilterOrdinals extends FilterBase {
    * {@inheritdoc}
    */
   public function process($text, $langcode) {
-    return new FilterProcessResult(_filter_ordinals($text));
+    return new FilterProcessResult(_filter_ordinals($text, $this));
   }
 
   /**
@@ -37,6 +38,18 @@ class FilterOrdinals extends FilterBase {
     else {
       return $this->t('Wraps ordinals (nd/st/th/rd) in &lt;span class="ordinal"&gt; tags.');
     }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    $form['more_filters_ordinals_tag_default'] = array(
+      '#type' => 'checkbox',
+      '#title' => $this->t('Wrap ordinals in &lt;sup&gt; tags instead of &lt;span class="ordinal"&gt; tags.'),
+      '#default_value' => $this->settings['more_filters_ordinals_tag_default'],
+    );
+    return $form;
   }
 
 }
